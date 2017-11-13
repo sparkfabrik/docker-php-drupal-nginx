df6fe1e15306c17f907e317763a60e128e6a7949
Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
2017-11-13 13:09:10 +0100

    limit just to our variables

--- 1.13.6-alpine/docker-entrypoint.sh
+++ 1.13.6-alpine/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 export PHP_HOST=${PHP_HOST:-php}
 export PHP_PORT=${PHP_PORT:-9000}
 export NGINX_PHP_READ_TIMEOUT=${NGINX_PHP_READ_TIMEOUT:-900}
-envsubst < /templates/default.conf > /etc/nginx/conf.d/default.conf
-envsubst < /templates/fastcgi.conf > /etc/nginx/fastcgi.conf
+envsubst '${PHP_HOST} ${PHP_PORT}' < /templates/default.conf > /etc/nginx/conf.d/default.conf
+envsubst '${NGINX_PHP_READ_TIMEOUT}' < /templates/fastcgi.conf > /etc/nginx/fastcgi.conf
 exec nginx -g "daemon off;"
