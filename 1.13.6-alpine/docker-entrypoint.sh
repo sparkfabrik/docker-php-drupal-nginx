0acedcf2075b56ba67fa783a369291899ad57090
Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
2017-12-10 19:35:44 +0100

    make the default server name configurable

--- 1.13.6-alpine/docker-entrypoint.sh
+++ 1.13.6-alpine/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 export PHP_HOST=${PHP_HOST:-php}
 export PHP_PORT=${PHP_PORT:-9000}
 export NGINX_PHP_READ_TIMEOUT=${NGINX_PHP_READ_TIMEOUT:-900}
-envsubst '${PHP_HOST} ${PHP_PORT}' < /templates/default.conf > /etc/nginx/conf.d/default.conf
+export NGINX_DEFAULT_SERVER_NAME=${NGINX_DEFAULT_SERVER_NAME:-drupal}
+envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME}' < /templates/default.conf > /etc/nginx/conf.d/default.conf
 envsubst '${NGINX_PHP_READ_TIMEOUT}' < /templates/fastcgi.conf > /etc/nginx/fastcgi.conf
 exec nginx -g "daemon off;"
