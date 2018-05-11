1807ee1f85b34f804879eadaf82c0fde9b295bf0
Alessio Piazza <alessiopiazza@gmail.com>
2018-05-11 17:16:11 +0200

    refs #1 - http to https conditional redirect

--- 1.13.6-alpine/docker-entrypoint.sh
+++ 1.13.6-alpine/docker-entrypoint.sh
@@ -4,6 +4,10 @@ export PHP_PORT=${PHP_PORT:-9000}
 export NGINX_PHP_READ_TIMEOUT=${NGINX_PHP_READ_TIMEOUT:-900}
 export NGINX_DEFAULT_SERVER_NAME=${NGINX_DEFAULT_SERVER_NAME:-drupal}
 export NGINX_DEFAULT_ROOT=${NGINX_DEFAULT_ROOT:-/var/www/html}
+export HTTPSREDIRECT=${HTTPSREDIRECT:-0}
 envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT}' < /templates/default.conf > /etc/nginx/conf.d/default.conf
 envsubst '${NGINX_PHP_READ_TIMEOUT}' < /templates/fastcgi.conf > /etc/nginx/fastcgi.conf
+if [ $HTTPSREDIRECT == 1 ]; then
+  sed  -e '/#httpsredirec/r /templates/httpsredirect.conf' -i /templates/default.conf;
+fi
 exec nginx -g "daemon off;"
