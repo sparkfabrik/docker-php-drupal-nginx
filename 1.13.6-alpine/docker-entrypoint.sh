85275b4ab796c7c6d38f16185199ce1797b5c2ee
Alessio Piazza <alessiopiazza@gmail.com>
2018-11-03 16:48:48 +0100

    nginx d8 conf

--- 1.13.6-alpine/docker-entrypoint.sh
+++ 1.13.6-alpine/docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [ $NGINX_HTTPSREDIRECT == 1 ]; then
   sed  -e '/#httpsredirec/r /templates/httpsredirect.conf' -i /templates/default.conf;
 fi
 envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT}' < /templates/default.conf > /etc/nginx/conf.d/default.conf
-if [ $NGINX_SUBFOLDER != 0] then
+if [ $NGINX_SUBFOLDER != 0]; then
   envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT} ${NGINX_SUBFOLDER}' < /templates/subfolder.conf > /etc/nginx/conf.d/default.conf
 fi
 envsubst '${NGINX_PHP_READ_TIMEOUT}' < /templates/fastcgi.conf > /etc/nginx/fastcgi.conf
