8747d1db8c445207079b27132bc5d132c7738e88
Paolo Mainardi <paolomainardi@gmail.com>
2018-11-23 16:23:28 +0100

    Rewrite fragments and auto escape NGINX_SUBFOLDER_ESCAPED

--- 1.13.6-alpine/docker-entrypoint.sh
+++ 1.13.6-alpine/docker-entrypoint.sh
@@ -6,13 +6,26 @@ export NGINX_DEFAULT_SERVER_NAME=${NGINX_DEFAULT_SERVER_NAME:-drupal}
 export NGINX_DEFAULT_ROOT=${NGINX_DEFAULT_ROOT:-/var/www/html}
 export NGINX_HTTPSREDIRECT=${NGINX_HTTPSREDIRECT:-0}
 export NGINX_SUBFOLDER=${NGINX_SUBFOLDER:-0}
+export NGINX_SUBFOLDER_ESCAPED=$(echo ${NGINX_SUBFOLDER} | sed 's/\//\\\//g')
 if [ $NGINX_HTTPSREDIRECT == 1 ]; then
   sed  -e '/#httpsredirec/r /templates/httpsredirect.conf' -i /templates/default.conf;
   sed  -e '/#httpsredirec/r /templates/httpsredirect.conf' -i /templates/subfolder.conf;
 fi
 envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT}' < /templates/default.conf > /etc/nginx/conf.d/default.conf
 if [ $NGINX_SUBFOLDER != 0 ]; then
-  envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT} ${NGINX_SUBFOLDER}' < /templates/subfolder.conf > /etc/nginx/conf.d/default.conf
+  envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT} ${NGINX_SUBFOLDER} ${NGINX_SUBFOLDER_ESCAPED}' < /templates/subfolder.conf > /etc/nginx/conf.d/default.conf
 fi
+
+# Rewrite fragments.
+for filename in /etc/nginx/conf.d/fragments/*.conf; do
+  if [ -e "${filename}" ] ; then
+    cp ${filename} ${filename}.tmp
+    envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT} ${NGINX_SUBFOLDER} ${NGINX_SUBFOLDER_ESCAPED}' < $filename.tmp > $filename
+    rm ${filename}.tmp
+  fi  
+done
+  
 envsubst '${NGINX_PHP_READ_TIMEOUT}' < /templates/fastcgi.conf > /etc/nginx/fastcgi.conf
 exec nginx -g "daemon off;"
+
+
