90c340f10d57621d08927a919a95d3c4036a0e2c
Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
2020-02-07 17:50:54 +0100

    fix gzip file copy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 export PHP_HOST=${PHP_HOST:-php}
 export PHP_PORT=${PHP_PORT:-9000}
 export NGINX_PHP_READ_TIMEOUT=${NGINX_PHP_READ_TIMEOUT:-900}
@@ -12,15 +13,15 @@ export NGINX_OSB_RESOLVER=${NGINX_OSB_RESOLVER:-8.8.8.8}
 export DRUPAL_PUBLIC_FILES_PATH=${DRUPAL_PUBLIC_FILES_PATH:-sites/default/files}
 export NGINX_CACHE_CONTROL_HEADER=${NGINX_CACHE_CONTROL_HEADER:-public,max-age=3600}
 export NGINX_GZIP_ENABLE=${NGINX_GZIP_ENABLE:-1}
-if [ $NGINX_HTTPSREDIRECT == 1 ]; then
+if [ ${NGINX_HTTPSREDIRECT} == 1 ]; then
   sed  -e '/#httpsredirec/r /templates/httpsredirect.conf' -i /templates/default.conf;
   sed  -e '/#httpsredirec/r /templates/httpsredirect.conf' -i /templates/subfolder.conf;
 fi
-if [ $NGINX_GZIP_ENABLE == 1 ]; then
-  cp templates/gzip.conf /etc/nginx/conf.d/gzip.conf
+if [ ${NGINX_GZIP_ENABLE} == 1 ]; then
+  cp /templates/gzip.conf /etc/nginx/conf.d/gzip.conf
 fi
 envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT}' < /templates/default.conf > /etc/nginx/conf.d/default.conf
-if [ $NGINX_SUBFOLDER != 0 ]; then
+if [ ${NGINX_SUBFOLDER} != 0 ]; then
   envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT} ${NGINX_SUBFOLDER} ${NGINX_SUBFOLDER_ESCAPED}' < /templates/subfolder.conf > /etc/nginx/conf.d/default.conf
 fi
 
