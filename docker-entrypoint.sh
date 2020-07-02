a0aba7ba781a36c63e8dfed1caebc1805c6d9d95
Noemi Mancini <noemi@noemimancini.com>
2020-07-02 17:10:43 +0200

    refs #000: dynamically write sitemap directive to robots.txt.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -37,11 +37,13 @@ envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_R
 if [ ${NGINX_SUBFOLDER} != 0 ]; then
   envsubst '${PHP_HOST} ${PHP_PORT} ${NGINX_DEFAULT_SERVER_NAME} ${NGINX_DEFAULT_ROOT} ${NGINX_SUBFOLDER} ${NGINX_SUBFOLDER_ESCAPED}' < /templates/subfolder.conf > /etc/nginx/conf.d/default.conf
 fi
-if [ -n "${SITEMAP_BASEURL}" ] && [ -w "${NGINX_DEFAULT_ROOT}/robots.txt" ] ; then
-  SITEMAP="$(grep -ic 'Sitemap' ${NGINX_DEFAULT_ROOT}/robots.txt)";
-  if [ "$SITEMAP" = 0 ] ; then
-    printf "\nSitemap: %s\n" ${SITEMAP_BASEURL} >> "${NGINX_DEFAULT_ROOT}/robots.txt";
-  fi
+
+# Handle robots.txt and sitemap directive
+ROBOTS_PATH=${NGINX_DEFAULT_ROOT}/robots.txt
+if [ -n "${SITEMAP_BASEURL}" ] && [ -w "${ROBOTS_PATH}" ] ; then
+	sed '/^Sitemap\:/d' ${ROBOTS_PATH} > ${ROBOTS_PATH}.sed; \
+		mv ${ROBOTS_PATH}.sed ${ROBOTS_PATH}
+	echo "Sitemap: ${SITEMAP_BASEURL}" >> "${NGINX_DEFAULT_ROOT}/robots.txt"
 fi
 
 # Rewrite main server fragments.
