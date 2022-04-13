6561fc677cd50720b600ba189b51f8f5b38dac2e
Daniele Monti <62102073+Monska85@users.noreply.github.com>
2022-04-13 15:28:38 +0200

    refs platform/1307: hide drupal route normalizer header (#74)

--- tests/html/index.php
+++ tests/html/index.php
@@ -4,5 +4,6 @@ header('X-Powered-By: test-file-php', true);
 header('X-Generator: test-file-php', true);
 header('X-Drupal-Dynamic-Cache: test-file-php', true);
 header('X-Drupal-Cache: test-file-php', true);
+header('X-Drupal-Route-Normalizer: 1', true);
 
 echo "<h1>It works</h1>";
